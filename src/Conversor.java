6bed75e06c413a21d8c2cffb7e6a54d67648b0d6
Miguel Romero <romerolaguado@gmail.com>
2022-12-19 18:13:28 -0500

    Se ha creado el programa con éxito pero, debe corregirse los decimales que salen por no ser algo recomendable en sistema bancario.

--- src/Conversor.java
+++ src/Conversor.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+
 public class Conversor {
     public static void main(String[] args) {
+        double digitarEuro, valorEnDolar;
 
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digitar la cantidad de euros obtenidos: ");
+        digitarEuro = scanner.nextDouble();
+        valorEnDolar = (digitarEuro * 1.06);
+
+        System.out.println("Tus " + digitarEuro + " euros equivalen a " + valorEnDolar + " US dolares.");
     }
 }
